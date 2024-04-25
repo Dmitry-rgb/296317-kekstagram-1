6bbd4d660caa0ecc9fa462b70371ec1c1e282e93
DmitryPotorochin <79530005262@yandex.ru>
2024-04-25 21:32:26 +0500

    123

--- js/main.js
+++ js/main.js
@@ -1,16 +1,6 @@
-<<<<<<< HEAD
-import {createOffers} from './data.js';
-import {pictures} from './rendering.js';
-
-// eslint-disable-next-line no-console
-console.log(createOffers(20));
-// eslint-disable-next-line no-console
-console.log(pictures);
-=======
 import { createOffers } from './data.js';
 import { renderOffers } from './render.js';
 
 const offers = createOffers(20);
 
 renderOffers(offers);
->>>>>>> 8fe540ddb737bb6f7cc6d9403e6fa61d01cc1dc0
